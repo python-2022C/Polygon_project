bbb2207af4e51355f81b046e69f0a253acc6a5e2
Jalilov <javohirjalilov777@gmail.com>
2022-07-27 15:08:04 +0500

    create triangle class

---
+++ triangle.py
@@ -0,0 +1,17 @@
+import math
+
+class Triangle:
+    def __init__(self, a:float, b:float, c:float):
+        pass
+
+# Create attribute "is_valid" in the Triangle class
+# This attribute checks if the triangle is valid
+# True if the triangle is valid, False otherwise
+
+# Create attribute "perimeter" in the Triangle class
+# This attribute finds the perimeter of the triangle
+# return perimeter of the triangle if the triangle is valid, 0 otherwise
+
+# Create attribute "area" in the Triangle class
+# This attribute finds the area of the triangle
+# return area of the triangle if the triangle is valid, 0 otherwise
