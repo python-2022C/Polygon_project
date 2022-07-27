f2ef24fa1284f054413ba1743f6b7ac056a0ff79
Jalilov <javohirjalilov777@gmail.com>
2022-07-27 15:08:17 +0500

    create circle class

---
+++ circle.py
@@ -0,0 +1,17 @@
+import math
+
+class Circle:
+    def __init__(self, radius:float):
+        pass
+
+# Create attribute "diameter" in the Circle class
+# This attribute finds the diameter of the circle
+# return diameter of the circle if the circle is valid, 0 otherwise
+
+# Create attribute "circumference" in the Circle class
+# This attribute finds the circumference of the circle
+# return circumference of the circle if the circle is valid, 0 otherwise
+
+# Create attribute "area" in the Circle class
+# This attribute finds the area of the circle
+# return area of the circle if the circle is valid, 0 otherwise
