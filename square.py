cc1814ee2e8d0b6f41a606cbe32a3f354a0fdbdf
Jalilov <javohirjalilov777@gmail.com>
2022-07-27 15:07:38 +0500

    create square class

---
+++ square.py
@@ -0,0 +1,17 @@
+import math
+
+class Square:
+    def __init__(self, square_side:float):
+        pass
+    
+# Create attribute "is_valid" in the Square class
+# This attribute checks if the square is valid
+# True if the square is valid, False otherwise
+
+# Create attribute "perimeter" in the Square class
+# This attribute finds the perimeter of the square
+# return perimeter of the square if the square is valid, 0 otherwise
+
+# Create attribute "area" in the Square class
+# This attribute finds the area of the square
+# return area of the square if the square is valid, 0 otherwise
