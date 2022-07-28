4355e9014c23a022abd4414858c0419705a11531
878 <majidovdiyorbek741@gmail.com>
2022-07-28 12:08:51 +0500

    triangle

--- triangle.py
+++ triangle.py
@@ -23,10 +23,8 @@ class Triangle:
 # Create method "perimeter" in the Triangle class
 # This method finds the perimeter of the triangle
 # return perimeter of the triangle if the triangle is valid, 0 otherwise
-    def perimeter(self) :
+    def perimeter(self):
         return self.a+self.b+self.c
-
-
 # Create method "area" in the Triangle class
 # This method finds the area of the triangle
 # return area of the triangle if the triangle is valid, 0 otherwise
