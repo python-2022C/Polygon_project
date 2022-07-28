24eceeb82bcfeed1451408fe28c9591d3fdc0dfc
tajriba <quvvatullayev@gmail.com>
2022-07-28 12:46:21 +0300

    .\square.py

--- square.py
+++ square.py
@@ -8,20 +8,27 @@ class Square:
         perimeter() -> float
         """
         self.square_side = square_side
+
     def is_valid(self):
         """
         returns area of square -> (bool)
         """        
         return self.square_side > 0
-    
-    def area(self):
+
+    def area(self)-> bool:
         """
         returns area of square -> (float)
         """
-        return float(self.square_side * self.square_side) if self.square_side > 0 else 0
+        if self.square_side > 0:
+            return float(self.square_side * self.square_side)
+        else:
+            return 0
     
     def perimeter(self):
         """
         returns peremets of square -> (float)
         """
-        return float(self.square_side * 4) if self.square_side > 0 else 0
+        if self.square_side > 0:
+            return float(self.square_side * 4)
+        else:
+            return 0
