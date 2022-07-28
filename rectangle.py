6a684063694f2f7d7b84e1a746de6076350a5c55
Azizbek Qozoqov <73469043+azizbekQozoqov@users.noreply.github.com>
2022-07-28 12:08:45 +0500

    Update rectangle.py

--- rectangle.py
+++ rectangle.py
@@ -8,27 +8,26 @@ class Rectangle:
     # Create method "is_valid" in the Rectangle class
     # This method checks if the rectangle is valid
     # True if the rectangle is valid, False otherwise
+    def is_valid(self):
+        """
+        returns area of rectangle -> (bool)
+        """ 
+        return self.a > 0 and self.b > 0
 
     # Create method "perimeter" in the Rectangle class
     # This method finds the perimeter of the rectangle
     # return perimeter of the rectangle if the rectangle is valid, 0 otherwise
+    def perimeter(self):
+        """
+        returns area of rectangle -> (float)
+        """
+        return float((self.a*2) + (self.b*2)) if self.is_valid() else 0
 
     # Create method "area" in the Rectangle class
     # This method finds the area of the rectangle
     # return area of the rectangle if the rectangle is valid, 0 otherwise
-    def is_valid(self):
-        if self.a == self.b:
-            return True
-        else:
-            False
-    def perimeter(self):
-        if self.is_valid():
-            return (self.a + self.b)*2
-        else:
-            return 0
     def area(self):
-        if self.is_valid():
-            return self.a * self.b
-        else:
-            return 0
-
+        """
+        returns peremets of rectangle -> (float)
+        """
+        return float(self.a * self.b) if self.is_valid() else 0
