23c8e1e1fdf7ec706530aec0e782109debed7a16
djumanov <diyorbekjumanov1120@gmail.com>
2022-07-28 14:08:42 +0500

    square.py: methods

--- square.py
+++ square.py
@@ -8,20 +8,39 @@ class Square:
         perimeter() -> float
         """
         self.square_side = square_side
-    def is_valid(self):
+    def is_valid(self) -> bool:
         """
-        returns area of square -> (bool)
+        This method checks if the circle is valid.
+
+        Args:
+            No
+        Returns:
+            bool: This method checks if the square is valid.
         """        
         return self.square_side > 0
     
     def area(self):
         """
-        returns area of square -> (float)
+        This method finds the area of the square.
+
+        Args:
+            No
+        Returns:
+            float or int: return area of the square if the square is valid, 0 otherwise
         """
-        return float(self.square_side * self.square_side) if self.square_side > 0 else 0
+        if self.is_valid(): 
+            return self.square_side * self.square_side
+        return 0
     
     def perimeter(self):
         """
-        returns peremets of square -> (float)
+        This method finds the perimeter of the square.
+
+        Args:
+            No
+        Returns:
+            float: return perimeter of the square if the square is valid, 0 otherwise
         """
-        return float(self.square_side * 4) if self.square_side > 0 else 0
+        if self.is_valid():
+            return self.square_side * 4
+        return 0
