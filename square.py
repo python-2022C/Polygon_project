c373cba9026b10ffb66089665f863ec18d30651f
sardorbek3112 <raupovsardorbek.3107@gmail.com>
2022-07-28 12:42:25 +0500

    square.py

--- square.py
+++ square.py
@@ -2,7 +2,19 @@ import math
 
 class Square:
     def __init__(self, square_side:float):
-        pass
+        self.square_side = square_side
+    def is_valid(self):
+        if self.square_side > 0:
+            return True
+        return False
+    def perimeter(self):
+        if self.is_valid():
+            return self.square_side * 4
+        return 0
+    def area(self):
+        if self.is_valid():
+            return pow(self.square_side,2)
+        return 0
     
 # Create method "is_valid" in the Square class
 # This method checks if the square is valid
