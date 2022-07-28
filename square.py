f6c3492de6810bfe702b0ee8ff0eccb050598637
AzizbekDev <azizbekqozoqovinfo@gmail.com>
2022-07-28 11:46:36 +0500

    Full solved square.py

--- square.py
+++ square.py
@@ -2,21 +2,26 @@ import math
 
 class Square:
     def __init__(self, square_side:float):
+        """
+        is_valid() -> bool
+        area() -> float
+        perimeter() -> float
+        """
         self.square_side = square_side
     def is_valid(self):
-        pass
+        """
+        returns area of square -> (bool)
+        """        
+        return self.square_side > 0
+    
     def area(self):
-        return self.square_side * self.square_side
+        """
+        returns area of square -> (float)
+        """
+        return float(self.square_side * self.square_side) if self.square_side > 0 else 0
+    
     def perimeter(self):
-        return self.square_side * 4
-# Create method "is_valid" in the Square class
-# This method checks if the square is valid
-# True if the square is valid, False otherwise
-
-# Create method "perimeter" in the Square class
-# This method finds the perimeter of the square
-# return perimeter of the square if the square is valid, 0 otherwise
-
-# Create method "area" in the Square class
-# This method finds the area of the square
-# return area of the square if the square is valid, 0 otherwise
+        """
+        returns peremets of square -> (float)
+        """
+        return float(self.square_side * 4) if self.square_side > 0 else 0
