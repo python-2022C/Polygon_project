b48c1e6ae959f13fd02d6f1a8f1a9237ad2613f3
tajriba <quvvatullayev@gmail.com>
2022-07-28 12:31:21 +0300

    .\triangle.py

--- triangle.py
+++ triangle.py
@@ -5,23 +5,33 @@ class Triangle:
         self.a=a 
         self.b=b
         self.c=c
-def is_valid(a,b,c):
-    l=a+b+c-max(a,b,c)-min(a,b,c)
-    if max(a,b,c)+min(a,b,c)>l :
-        return True
-    return False
 
-# Create method "is_valid" in the Triangle class
-# This method checks if the triangle is valid
-# True if the triangle is valid, False otherwise
+        # Create method "is_valid" in the Triangle class
+        # This method checks if the triangle is valid
+        # True if the triangle is valid, False otherwise
 
-# Create method "get_type" in the Triangle class
-# This method finds the type of the triangle
+        # Create method "get_type" in the Triangle class
+        # This method finds the type of the triangle
 
-# Create method "perimeter" in the Triangle class
-# This method finds the perimeter of the triangle
-# return perimeter of the triangle if the triangle is valid, 0 otherwise
+        # Create method "perimeter" in the Triangle class
+        # This method finds the perimeter of the triangle
+        # return perimeter of the triangle if the triangle is valid, 0 otherwise
 
-# Create method "area" in the Triangle class
-# This method finds the area of the triangle
-# return area of the triangle if the triangle is valid, 0 otherwise
+        # Create method "area" in the Triangle class
+        # This method finds the area of the triangle
+        # return area of the triangle if the triangle is valid, 0 otherwise
+
+    def is_valid(self):
+        l=self.a+self.b+self.c-max(self.a,self.b,self.c)-min(self.a,self.b,self.c)
+        if max(self.a,self.b,self.c)+min(self.a,self.b,self.c) > l :
+            return True
+        else:
+            return False
+    def get_type(self):
+        if self.c**2 == self.a**2 + self.b**2:
+            return 'right angle'
+        elif self.b == self.c:
+            return 'equilateral'
+        elif self.c**2 == self.a**2 == self.b**2:
+            return "equal side"
+        
