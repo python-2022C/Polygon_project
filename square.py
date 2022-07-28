d421a2f6ab400b4415788a3c3fd2956aa33b6056
AzizbekDev <azizbekqozoqovinfo@gmail.com>
2022-07-28 11:38:56 +0500

    Added  methods square.py

--- square.py
+++ square.py
@@ -2,13 +2,13 @@ import math
 
 class Square:
     def __init__(self, square_side:float):
+        self.square_side = square_side
+    def is_valid(self):
         pass
-    def is_valid():
-        return ""
     def area(self):
-        return 0
+        return self.square_side * self.square_side
     def perimeter(self):
-        return 0
+        return self.square_side * 4
 # Create method "is_valid" in the Square class
 # This method checks if the square is valid
 # True if the square is valid, False otherwise
